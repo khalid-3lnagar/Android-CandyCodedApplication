596b6866ad707e73b8e1ec1ed28966775b5ae301
Khalid Elnagar <khalid.3lnagar@gmail.com>
2019-10-24 14:34:12 +0200

    add: Start the Google Maps Activity Task

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,14 +1,17 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 public class InfoActivity extends AppCompatActivity {
-
+    public static final String STORE_GEO_LOCATION = "geo:0,0?q=618 E South St Orlando, FL 32801";
+    public static final String GOOGLE_MAPS_PACKAGE = "com.google.android.apps.maps";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +25,13 @@ public class InfoActivity extends AppCompatActivity {
 
 
     }
-
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
-
+    public void createMapIntent(View view) {
+        Uri starLocationUri = Uri.parse(STORE_GEO_LOCATION);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, starLocationUri);
+        mapIntent.setPackage(GOOGLE_MAPS_PACKAGE);
+        if (mapIntent.resolveActivity(getPackageManager()) != null)
+            startActivity(mapIntent);
+    }
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
